582339cb5f600f9e9b37f75819689b58e073f9dc
whadam <asd123fg2@gmail.com>
2021-09-12 16:24:23 +0900

    number sort 1

---
+++ 2750/Main.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n;
+        int[] arr;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            bw.write(Integer.toString(arr[i]) + "\n");
+        }
+
+        bw.close();
+    }
+}
