7ddd15dcc8f834437314c2d974293dcb0bb10a42
whadam <asd123fg2@gmail.com>
2021-09-17 21:49:40 +0900

    14889

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -4,12 +4,16 @@ import java.util.StringTokenizer;
 public class Main {
     static int n, result;
     static int[][] stat;
+    static boolean[] matched;
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
+        stat = new int[n][n];
+        matched = new boolean[n];
+
         StringTokenizer st;
 
         for (int i = 0; i < n; i++) {
@@ -19,13 +23,15 @@ public class Main {
             }
         }
 
-        matching();
+        matching(0,0);
 
         bw.close();
     }
 
-    static void matching(int start, int link) {
+    static void matching(int idx, int cnt) {
+        if (cnt == n/2) {
 
+        }
     }
 
 }
