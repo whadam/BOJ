b8d6d103a1d9deef08625ceb4df175016bcc80e1
whadam <asd123fg2@gmail.com>
2021-09-18 21:51:20 +0900

    1904

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,37 +1,25 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int n, result;
-    static int[][] stat;
-    static boolean[] matched;
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
+        int n;
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+
         n = Integer.parseInt(br.readLine());
-        stat = new int[n][n];
-        matched = new boolean[n];
 
-        StringTokenizer st;
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                stat[i][j] = Integer.parseInt(st.nextToken());
-            }
+        while (n-- > 2) {
+            arr[2] = (arr[0]+arr[1])%15746;
+            arr[0] = arr[1];
+            arr[1] = arr[2];
         }
 
-        matching(0,0);
-
+        bw.write(arr[2]+"");
         bw.close();
     }
-
-    static void matching(int idx, int cnt) {
-        if (cnt == n/2) {
-
-        }
-    }
-
 }
