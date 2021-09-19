23816dfc155692bd0be24e8dc63752612f6f9b70
whadam <asd123fg2@gmail.com>
2021-09-19 21:52:50 +0900

    1932

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] rgb;
+    static int[][] tri;
     static int[][] cost;
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,41 +13,27 @@ public class Main {
         StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
-        rgb = new int[n][3];
-        cost = new int[n][3];
+        tri = new int[n][n];
+        cost = new int[n][n];
 
         for (i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
-            for (j = 0; j < 3; j++) {
-                rgb[i][j] = Integer.parseInt(st.nextToken());
+            for (j = 0; j < i+1; j++) {
+                tri[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        cost[0] = rgb[0];
 
-//        bw.write(Math.min(calc(n-1,0), Math.min(calc(n-1,1), calc(n-1,2)))+"");
-        bw.write(calc(n-1,0)+"");
+        bw.write(calc(n-1) + "");
         bw.close();
     }
 
-    static int calc(int n, int color) {
-        int min;
-        if (cost[n][color] == 0) {
-            if (color == 0) {
-                min = Math.min(calc(n-1, 1), calc(n-1, 2));
-                cost[n][0] = min + rgb[n][0];
-            }
-            else if (color == 1) {
-                min = Math.min(calc(n-1, 0), calc(n-1, 2));
-                cost[n][1] = min + rgb[n][1];
-            }
-            else {
-                min = Math.min(calc(n-1, 0), calc(n-1, 1));
-                cost[n][0] = min + rgb[n][2];
-            }
-        }
+    static int calc(int n) {
+        int max = Integer.MIN_VALUE;
 
-        return cost[n][color];
+
+
+        return max;
     }
 }
