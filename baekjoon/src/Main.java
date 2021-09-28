2a41b1291bbcdd44f7fd753678dff969792a698e
whadam <asd123fg2@gmail.com>
2021-09-28 17:34:25 +0900

    11053

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,26 +1,31 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, min = Integer.MAX_VALUE;
+    static int n;
+    static int[] a;
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        int i;
+        int i, cnt = 1, big, tmp;
 
         n = Integer.parseInt(br.readLine());
+        a = new int[n];
 
-        calc(1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        big = Integer.parseInt(st.nextToken());
 
-        bw.write(min + "");
+        for (i = 1; i < n; i++) {
+            tmp = Integer.parseInt(st.nextToken());
+            if (big < tmp) {
+                big = tmp;
+                cnt++;
+            }
+        }
+
+        bw.write(cnt+"");
         bw.close();
-    }
-
-    static int calc(int val) {
-
-
-
-        return 0;
     }
 }
