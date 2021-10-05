63a5db8dace2ef67c6d8c9bf5485a0db229740f3
whadam <asd123fg2@gmail.com>
2021-10-05 17:50:19 +0900

    12865

--- baekjoon/src/Main.java
+++ baekjoon/src/Main.java
@@ -1,65 +1,43 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, i, max = 0;
-    static Integer[] asc, desc;
-    static int[] seq;
+    static int n, k;
+    static Integer[][] item;
+    static int[] W, V;
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        seq = new int[n];
-        asc = new Integer[n];
-        desc = new Integer[n];
-
+        int i;
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (i = 0; i < n; i++) {
-            seq[i] = Integer.parseInt(st.nextToken());
-        }
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        item = new Integer[n][2];   // 0 weight, 1 value
+        W = new int[n];
+        V = new int[n];
 
         for (i = 0; i < n; i++) {
-            ASC(i);
-            DESC(i);
+            st = new StringTokenizer(br.readLine());
+            item[i][0] = Integer.parseInt(st.nextToken());
+            item[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (i = 0; i < n; i++) {
-            asc[i] += desc[i]-1;
-            max = Math.max(asc[i], max);
-        }
+        sum(0, k);
 
-        bw.write(max+"");
         bw.close();
     }
 
-    static int ASC(int idx) {
-        if (asc[idx] == null) {
-            asc[idx] = 1;
+    static int sum(int i, int k) {
+        if (i < 0) return 0;
 
-            for (i = idx - 1; i >= 0; i--) {
-                if (seq[i] < seq[idx]) {
-                    asc[idx] = Math.max(asc[idx], ASC(i) + 1);
-                }
-            }
+        if (item[i][k] == null) {
+
         }
-
-        return asc[idx];
-    }
-
-    static int DESC(int idx) {
-        if (desc[idx] == null) {
-            desc[idx] = 1;
-
-            for (i = idx + 1; i < n; i++) {
-                if (seq[i] > seq[idx]) {
-                    desc[idx] = Math.max(desc[idx], DESC(i) + 1);
-                }
-            }
-        }
-
-        return desc[idx];
+        return 0;
     }
 }
